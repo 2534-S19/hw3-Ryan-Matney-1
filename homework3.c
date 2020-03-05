38c20ecdedef00359cdbc7d48654d5031581f60b
matne <matne@DESKTOP-RTFM5UA>
2020-03-05 16:53:21 -0500

    everything fully working besides FSM

--- homework3.c
+++ homework3.c
@@ -3,6 +3,8 @@
 #include "myGPIO.h"
 #include "myTimer.h"
 
+#define UNPRESSED   0x00
+
 int main(void)
 {
     // Count variables to control the LEDs.
@@ -10,18 +12,15 @@ int main(void)
     unsigned int count1 = 0;
 
     // TODO: Declare the variables that main uses to interact with your state machine.
-
+    unsigned char buttonhistory = UNPRESSED;
 
     // Stops the Watchdog timer.
     initBoard();
     // Initialize the GPIO.
-    // YOU MUST WRITE THIS FUNCTION IN myGPIO.c
     initGPIO();
     // Initialize Timer0 to provide a one second count interval for updating LED2.
-    // YOU MUST WRITE THIS FUNCTION IN myTimer.c
     initTimer(TIMER32_0_BASE, TIMER0_PRESCALER, TIMER0_COUNT);
     // Initialize Timer1 to provide a one millisecond count interval for updating the button history.
-    // YOU MUST WRITE THIS FUNCTION IN myTimer.c
     initTimer(TIMER32_1_BASE, TIMER1_PRESCALER, TIMER1_COUNT);
 
     while(1)
@@ -35,21 +34,27 @@ int main(void)
 
         // TODO: If Timer0 has expired, increment count0.
         // YOU MUST WRITE timer0expired IN myTimer.c
-
-
-
+        if (timer0Expired())
+        {
+            count0++;
+        }
         // TODO: If Timer1 has expired, update the button history from the pushbutton value.
         // YOU MUST WRITE timer1expired IN myTimer.c
-
-
+        if (timer1Expired())
+        {
+            checkStatus_BoosterpackS1();
+        }
 
         // TODO: Call the button state machine function to check for a completed, debounced button press.
         // YOU MUST WRITE THIS FUNCTION BELOW.
 
-
+        fsmBoosterpackButtonS1(buttonhistory);
 
         // TODO: If a completed, debounced button press has occurred, increment count1.
-
+        if (fsmBoosterpackButtonS1(buttonhistory))
+        {
+            count1++;
+        }
 
 
     }
@@ -64,22 +69,128 @@ void initBoard()
 // Since count is an unsigned integer, you can mask the value in some way.
 void changeLaunchpadLED2(unsigned int count)
 {
-
+    switch(count & 7)
+    {
+        case 0:
+            turnOn_LaunchpadLED2Red();
+            turnOff_LaunchpadLED2Green();
+            turnOff_LaunchpadLED2Blue();
+            break;
+        case 1:
+            turnOff_LaunchpadLED2Red();
+            turnOn_LaunchpadLED2Green();
+            turnOff_LaunchpadLED2Blue();
+            break;
+        case 2:
+            turnOn_LaunchpadLED2Red();
+            turnOn_LaunchpadLED2Green();
+            turnOff_LaunchpadLED2Blue();
+            break;
+        case 3:
+            turnOff_LaunchpadLED2Red();
+            turnOff_LaunchpadLED2Green();
+            turnOn_LaunchpadLED2Blue();
+            break;
+        case 4:
+            turnOn_LaunchpadLED2Red();
+            turnOff_LaunchpadLED2Green();
+            turnOn_LaunchpadLED2Blue();
+            break;
+        case 5:
+            turnOff_LaunchpadLED2Red();
+            turnOn_LaunchpadLED2Green();
+            turnOn_LaunchpadLED2Blue();
+            break;
+        case 6:
+            turnOn_LaunchpadLED2Red();
+            turnOn_LaunchpadLED2Green();
+            turnOn_LaunchpadLED2Blue();
+            break;
+        case 7:
+            turnOff_LaunchpadLED2Red();
+            turnOff_LaunchpadLED2Green();
+            turnOff_LaunchpadLED2Blue();
+            break;
+        default:
+            turnOff_LaunchpadLED2Red();
+            turnOff_LaunchpadLED2Green();
+            turnOff_LaunchpadLED2Blue();
+    }
 }
 
 // TODO: Maybe the value of a count variable to a color for the Boosterpack LED
 // This is essentially a copy of the previous function, using a different LED
 void changeBoosterpackLED(unsigned int count)
 {
-
+    switch(count & 7)
+    {
+        case 0:
+            turnOn_BoosterpackLEDRed();
+            turnOff_BoosterpackLEDGreen();
+            turnOff_BoosterpackLEDBlue();
+            break;
+        case 1:
+            turnOff_BoosterpackLEDRed();
+            turnOn_BoosterpackLEDGreen();
+            turnOff_BoosterpackLEDBlue();
+            break;
+        case 2:
+            turnOn_BoosterpackLEDRed();
+            turnOn_BoosterpackLEDGreen();
+            turnOff_BoosterpackLEDBlue();
+            break;
+        case 3:
+            turnOff_BoosterpackLEDRed();
+            turnOff_BoosterpackLEDGreen();
+            turnOn_BoosterpackLEDBlue();
+            break;
+        case 4:
+            turnOn_BoosterpackLEDRed();
+            turnOff_BoosterpackLEDGreen();
+            turnOn_BoosterpackLEDBlue();
+            break;
+        case 5:
+            turnOff_BoosterpackLEDRed();
+            turnOn_BoosterpackLEDGreen();
+            turnOn_BoosterpackLEDBlue();
+            break;
+        case 6:
+            turnOn_BoosterpackLEDRed();
+            turnOn_BoosterpackLEDGreen();
+            turnOn_BoosterpackLEDBlue();
+            break;
+        case 7:
+            turnOff_BoosterpackLEDRed();
+            turnOff_BoosterpackLEDGreen();
+            turnOff_BoosterpackLEDBlue();
+            break;
+        default:
+            turnOff_BoosterpackLEDRed();
+            turnOff_BoosterpackLEDGreen();
+            turnOff_BoosterpackLEDBlue();
+    }
 }
 
 // TODO: Create a button state machine.
 // The button state machine should return true or false to indicate a completed, debounced button press.
-bool fsmBoosterpackButtonS1(unsigned int buttonhistory)
+bool fsmBoosterpackButtonS1(unsigned char buttonhistory)
 {
     bool pressed = false;
+    typedef enum {s0, s1, s3} buttonState;
+    static buttonState currentState = s0;
+
+    switch(currentState)
+    {
+        case s0:
+            if (buttonhistory != UNPRESSED)
+                currentState = s1;
+            break;
+        case s1:
+            if (buttonhistory = UNPRESSED)
+                currentState = s3;
+            break;
 
 
+    }
     return pressed;
 }
--- myGPIO.c
+++ myGPIO.c
@@ -1,8 +1,8 @@
 /*
  * GPIO.c
  *
- *  Created on:
- *      Author:
+ *  Created on: 3/5/20
+ *      Author: Kennith Matney
  */
 
 // For the code you place here, you may use your code that uses register references.
@@ -11,140 +11,165 @@
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 #include "myGPIO.h"
 
+/* Defining the bits associated with the Pushbuttons and LED's */
+#define LAUNCHPAD_S1 BIT1
+#define LAUNCHPAD_S2 BIT4
+#define BOOSTERPACK_S1 BIT1
+#define BOOSTERPACK_S2 BIT5
+
+#define LED1 BIT0
+
+#define LED2_RED BIT0
+#define LED2_GREEN BIT1
+#define LED2_BLUE BIT2
+
+#define BLED_RED BIT6
+#define BLED_GREEN BIT4
+#define BLED_BLUE BIT6
+
 // TODO: Create a function to initialize the GPIO.
 // Even though this assignment does not use all of the pushbuttons, you should write one function that does a complete GPIO init.
 void initGPIO()
 {
-    // Launchpad S1
+    P1DIR &= ~LAUNCHPAD_S2; //makes bit 4 of pin 1 an input
+    P1REN |= LAUNCHPAD_S2; //makes bit 4 of pin 1 able to use a pull up/down resistor
+    P1OUT |= LAUNCHPAD_S2; //makes bit 4 of pin 1 use a pull up resistor
 
-    // Launchpad S2
+    P1DIR &= ~LAUNCHPAD_S1; //makes bit 1 of pin 1 an input
+    P1REN |= LAUNCHPAD_S1; //makes bit 1 of pin 1 able to use a pull up/down resistor
+    P1OUT |= LAUNCHPAD_S1; //makes bit 1 of pin 1 use a pull up resistor
 
-    // Boosterpack S1
+    P3DIR &= ~BOOSTERPACK_S2; //makes bit 5 of pin 3 an input
+    P3REN |= BOOSTERPACK_S2; //makes bit 5 of pin 3 able to use a pull up/down resistor
+    P3OUT |= BOOSTERPACK_S2; //makes bit 5 of pin 3 use a pull up resistor
 
-    // Boosterpack S2
+    P5DIR &= ~BOOSTERPACK_S1; //makes bit 1 of pin 5 an input
+    P5REN |= BOOSTERPACK_S1; //makes bit 1 of pin 5 able to use a pull up/down resistor
+    P5OUT |= BOOSTERPACK_S1; //makes bit 1 of pin 5 use a pull up resistor
 
-    // Launchpad LED1
+    P1DIR |= LED1; //makes bit 0 of pin 1 as an LED output
+    P2DIR |= BLED_RED; //makes bit 6 of pin 2 as an LED output
+    P2DIR |= BLED_GREEN; //makes bit 4 of pin 2 as an LED output
+    P5DIR |= BLED_BLUE; //makes bit 6 of pin 5 as an LED output
+    P2DIR |= LED2_RED; //makes bit 0 of pin 2 as an LED output
+    P2DIR |= LED2_GREEN; //makes bit 1 of pin 2 as an LED output
+    P2DIR |= LED2_BLUE; //makes bit 2 of pin 2 as an LED output
 
-    // Launchpad LED2 Red
+    // Initializing all LED's to be turned off
+    P1OUT &= ~LED1;
+    P2OUT &= ~BLED_RED;
+    P2OUT &= ~BLED_GREEN;
+    P5OUT &= ~BLED_BLUE;
+    P2OUT &= ~LED2_RED;
+    P2OUT &= ~LED2_GREEN;
+    P2OUT &= ~LED2_BLUE;
 
-    // Launchpad LED2 Green
-
-    // Launchpad LED2 Blue
-
-    // Boosterpack LED Red
-
-    // Boosterpack LED Green
-
-    // Boosterpack LED Blue
-
-
-    // Turn off all LEDs at the start.
 }
 
 // TODO: Create a function to return the status of Launchpad Pushbutton S1
 unsigned char checkStatus_LaunchpadS1()
 {
-
+    return ((P1IN & LAUNCHPAD_S1) == LAUNCHPAD_S1);
 }
 
 // TODO: Create a function to return the status of Launchpad Pushbutton S2
 unsigned char checkStatus_LaunchpadS2()
 {
-
+    return ((P1IN & LAUNCHPAD_S2) == LAUNCHPAD_S2);
 }
 
 // TODO: Create a function to return the status of Boosterpack Pushbutton S1
 unsigned char checkStatus_BoosterpackS1()
 {
-
+    return ((P5IN & BOOSTERPACK_S1) == BOOSTERPACK_S1);
 }
 
 // TODO: Create a function to return the status of Boosterpack Pushbutton S2
 unsigned char checkStatus_BoosterpackS2()
 {
-
+    return ((P3IN & BOOSTERPACK_S2) == BOOSTERPACK_S2);
 }
 
 // TODO: Create a function to turn on Launchpad LED1.
 void turnOn_LaunchpadLED1()
 {
-
+    P1OUT |= LED1;
 }
 
 // TODO: Create a function to turn off Launchpad LED1.
 void turnOff_LaunchpadLED1()
 {
-
+    P1OUT &= ~LED1;
 }
 
 // TODO: Create a function to turn on the Red Launchpad LED2.
 void turnOn_LaunchpadLED2Red()
 {
-
+    P2OUT |= LED2_RED;
 }
 
 // TODO: Create a function to turn off the Red Launchpad LED2.
 void turnOff_LaunchpadLED2Red()
 {
-
+    P2OUT &= ~LED2_RED;
 }
 
 // TODO: Create a function to turn on the Green Launchpad LED2.
 void turnOn_LaunchpadLED2Green()
 {
-
+    P2OUT |= LED2_GREEN;
 }
 
 // TODO: Create a function to turn off the Green Launchpad LED2.
 void turnOff_LaunchpadLED2Green()
 {
-
+    P2OUT &= ~LED2_GREEN;
 }
 
 // TODO: Create a function to turn on the Blue Launchpad LED2.
 void turnOn_LaunchpadLED2Blue()
 {
-
+    P2OUT |= LED2_BLUE;
 }
 
 // TODO: Create a function to turn off the Blue Launchpad LED2.
 void turnOff_LaunchpadLED2Blue()
 {
-
+    P2OUT &= ~LED2_BLUE;
 }
 
 // TODO: Create a function to turn on the Red Boosterpack LED2.
 void turnOn_BoosterpackLEDRed()
 {
-
+    P2OUT |= BLED_RED;
 }
 
 // TODO: Create a function to turn off the Red Boosterpack LED2.
 void turnOff_BoosterpackLEDRed()
 {
-
+    P2OUT &= ~BLED_RED;
 }
 
 // TODO: Create a function to turn on the Green Boosterpack LED2.
 void turnOn_BoosterpackLEDGreen()
 {
-
+    P2OUT |= BLED_GREEN;
 }
 
 // TODO: Create a function to turn off the Green Boosterpack LED2.
 void turnOff_BoosterpackLEDGreen()
 {
-
+    P2OUT &= ~BLED_GREEN;
 }
 
 // TODO: Create a function to turn on the Blue Boosterpack LED2.
 void turnOn_BoosterpackLEDBlue()
 {
-
+    P5OUT |= BLED_BLUE;
 }
 
 // TODO: Create a function to turn off the Green Boosterpack LED2.
 void turnOff_BoosterpackLEDBlue()
 {
-
+    P5OUT &= ~BLED_BLUE;
 }
